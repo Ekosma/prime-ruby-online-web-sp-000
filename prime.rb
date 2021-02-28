23de9c1be0985914fe73f05cecf238f0f8b2997a
Evelyn Kosma <ezranaja@gmail.com>
2021-02-28 19:20:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,4 @@
 def prime?(num)
-  if num <= 1
-    return false
-  elsif num <= 3
-    return true
-  elsif num % 2 == 0 
-    return false
-  elsif num % 41 == 0
-    return false
-  elsif num % 33 == 0
-    return false 
-  else 
-    true
-  end
+ num.each {|num| 
+   end
 end
