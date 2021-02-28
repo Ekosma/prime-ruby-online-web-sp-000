cfe0e0d4a3faa68dfce937b2c351546f992d0eff
Evelyn Kosma <ezranaja@gmail.com>
2021-02-28 19:35:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(num)
   ret = true
  array_of_num = (2.. num - 1)
  array_of_num.each do |i|  
-  if num % i == 0 
-    ret = false
+    if num % i == 0 
+      ret = false
     end
   end
 end
